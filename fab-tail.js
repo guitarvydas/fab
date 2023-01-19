87474006551615324ccc193780e56f64844da020
Paul Tarvydas <guitarvydas@gmail.com>
2023-01-19 08:59:34 -0500

    break - not failing but not correct

--- fab-tail.js
+++ fab-tail.js
@@ -1,33 +1,19 @@
 var argv;
 
+// Component Based Programming version
 function main () {
     argv = require('yargs/yargs')(process.argv.slice(2)).argv;
-    if (argv.view) {
-	viewGeneratedCode = true;
-    } else {
-	viewGeneratedCode = false;
-    }
-    if (argv.trace) {
-	tracing = true;
-	traceDepth = 0;
-    } else {
-	tracing = false;
-    }
-    var srcfilename = argv._[0];
-    if (srcfilename === '-') {
-	srcfilename = '/dev/fd/0';
-    }
-    var grammarName = argv._[1];
-    var grammarfilename = argv._[2];
-    var fmtfilename = argv._[3];
+    viewGeneratedCode = false;
+    tracing = false;
+    traceDepth = 0;
+    srcfilename = '/dev/fd/0';
+    var grammarfilename = argv._[1];
+    var fmtfilename = argv._[2];
     var src = fs.readFileSync (srcfilename, 'utf-8');
     var grammar = fs.readFileSync (grammarfilename, 'utf-8');
     var fmt = fs.readFileSync (fmtfilename, 'utf-8');
-    if (argv.support) {
-	var r = transpile (src, grammarName, grammar, fmt, ohm, compilefmt, argv.support);
-    } else {
-	var r = transpile (src, grammarName, grammar, fmt, ohm, compilefmt, undefined);
-    }
+    var support = argv._[3];
+    var r = transpile (src, grammarName, grammar, fmt, ohm, compilefmt, argv.support);
     var success = r [0]
     var transpiled = r [1]
     var errormessage = r [2]
